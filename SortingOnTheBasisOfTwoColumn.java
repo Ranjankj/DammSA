07ee3a8d4582631a8ff638331ab0774c2c9dc430
Ranjan <63071934+Ranjankj@users.noreply.github.com>
2022-05-25 12:49:06 +0530

    Create SortingOnTheBasisOfTwoColumn.java

---
+++ SortingOnTheBasisOfTwoColumn.java
@@ -0,0 +1,16 @@
+//Sorting based on column2 (ascending)
+
+ Arrays.sort(arr, (a,b)-> {
+            if(a[1] == b[1]){
+                return a[0] - b[0];
+         }
+         return a[1] - b[1];
+  });
+//Sorting based on column2 (descending)
+
+Arrays.sort(arr, (a,b)-> {
+            if(b[1] == a[1]){
+                return b[0] - a[0];
+         }
+         return b[1] - a[1];
+  });
